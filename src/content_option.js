db55b381e9c60964cea10ebe6d08cc76d1d4cad5
Gareth <ggreenaway053@hotmail.com>
2024-04-05 16:03:03 +0100

    Delete src directory

--- src/content_option.js
+++
@@ -1,8 +0,0 @@
-const meta = {
-  title: "Reach | Effortlessly connect with anyone. ",
-  description: "Reach simplifies contact management by offering a user-friendly and comprehensive phonebook app. Reach puts an end to the frustration of lost or outdated contact information, ensuring you always have the most up-to-date details at your fingertips.",
-};
-
-export {
-  meta
-};
